6d02546fae09e0122c04642851ee20e72695ad95
Hamza Bouziane <hbouziane@smb.sqli.com>
2022-12-05 17:19:24 +0100

    test non-null user case

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,6 +1,9 @@
 package org.craftedsw.tripservicekata.trip;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
@@ -10,15 +13,29 @@ public class TripServiceTest {
   @Test
   void should_throw_exception_if_not_logged_user() {
 
-    Assertions.assertThatThrownBy(() -> new TripServiceMock().getTripsByUser(null))
+    assertThatThrownBy(() -> new TripServiceMockWithNullUser().getTripsByUser(null))
         .isInstanceOf(UserNotLoggedInException.class);
   }
 
-  class TripServiceMock extends TripService {
+  @Test
+  void should_log_user() {
+    List<Trip> trips = new TripServiceMockWithUser().getTripsByUser(new User());
+    assertThat(trips).isEmpty();
+  }
+
+  static class TripServiceMockWithNullUser extends TripService {
 
     @Override
     public User getLoggedUser() {
       return null;
+    }
+  }
+
+  static class TripServiceMockWithUser extends TripService {
+
+    @Override
+    public User getLoggedUser() {
+      return new User();
     }
   }
 }
