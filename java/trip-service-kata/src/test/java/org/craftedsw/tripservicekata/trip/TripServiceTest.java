7c6ccffa19e144653c8f95afcc7805b015f1f7e7
FAYCAL IHABRITANE <adm-azr-faycal.ihabritane>
2022-12-05 17:07:33 +0100

    First junit test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.assertj.core.api.Assertions;
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+  @Test
+  void should_throw_exception_if_not_logged_user() {
+
+    Assertions.assertThatThrownBy(() -> new TripServiceMock().getTripsByUser(null))
+        .isInstanceOf(UserNotLoggedInException.class);
+  }
+
+  class TripServiceMock extends TripService {
+
+    @Override
+    public User getLoggedUser() {
+      return null;
+    }
+  }
 }
