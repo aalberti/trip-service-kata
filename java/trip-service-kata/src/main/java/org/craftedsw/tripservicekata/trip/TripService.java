03e7a74937dab9095de11ee5821feb7a2a4f17cc
Yassir Aghraba <yaghraba@sqli.com>
2022-12-05 15:00:43 +0100

    minor refactor for getTrips method

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,44 +2,36 @@ package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+  public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 
-		User loggedUser = getLoggedUser();
-		if (loggedUser != null) {
-			return getTripsForLoggedUser(user, loggedUser);
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    User loggedUser = getLoggedUser();
+    if (loggedUser != null) {
+      return getTrips(user, loggedUser);
+    } else {
+      throw new UserNotLoggedInException();
+    }
+  }
 
-	private List<Trip> getTripsForLoggedUser(User user, User loggedUser) {
-		boolean isFriend = false;
-		for (User friend : user.getFriends()) {
-			if (friend.equals(loggedUser)) {
-				isFriend = true;
-				break;
-			}
-		}
-		List<Trip> tripList = new ArrayList<>();
-		if (isFriend) {
-			tripList = findTripsByUser(user);
-		}
-		return tripList;
-	}
+  private List<Trip> getTrips(User user, User loggedUser) {
+    boolean hasLoggedUserAsFriend = user.getFriends().stream().anyMatch(f -> f.equals(loggedUser));
+    if (hasLoggedUserAsFriend) {
+      return findTripsByUser(user);
+    } else {
+      return new ArrayList<>();
+    }
+  }
 
-	List<Trip> findTripsByUser(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+  List<Trip> findTripsByUser(User user) {
+    return TripDAO.findTripsByUser(user);
+  }
 
-	User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
-
+  User getLoggedUser() {
+    return UserSession.getInstance().getLoggedUser();
+  }
 }
