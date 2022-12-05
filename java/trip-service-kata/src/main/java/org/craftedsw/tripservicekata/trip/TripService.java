f5e4e1556afddf49001bb0e0e338ea0478a2a6b5
ylechqar <adm-azr-youssef.lechqar@chanel.com>
2022-12-05 17:32:14 +0100

    user with friends ..

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,7 +2,6 @@ package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
@@ -10,7 +9,7 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
   public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-    List<Trip> tripList = new ArrayList<Trip>();
+    List<Trip> tripList = new ArrayList<>();
     User loggedUser = getLoggedUser();
     boolean isFriend = false;
     if (loggedUser != null) {
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -23,6 +23,26 @@ public class TripServiceTest {
     assertThat(trips).isEmpty();
   }
 
+  @Test
+  void with_friend() {
+
+    User user = new User();
+    user.addFriend(new User());
+    List<Trip> trips = new TripServiceMockWithUser().getTripsByUser(user);
+    assertThat(trips).isEmpty();
+  }
+
+  @Test
+  void with_friend2() {
+
+    User user = new User();
+    TripServiceMockWithUser tripServiceMockWithUser = new TripServiceMockWithUser();
+    User loggedUser = tripServiceMockWithUser.getLoggedUser();
+    user.addFriend(loggedUser);
+    List<Trip> trips = tripServiceMockWithUser.getTripsByUser(user);
+    assertThat(trips).isEmpty();
+  }
+
   static class TripServiceMockWithNullUser extends TripService {
 
     @Override
@@ -33,9 +53,11 @@ public class TripServiceTest {
 
   static class TripServiceMockWithUser extends TripService {
 
+    User loggedUser = new User();
+
     @Override
     public User getLoggedUser() {
-      return new User();
+      return loggedUser;
     }
   }
 }
