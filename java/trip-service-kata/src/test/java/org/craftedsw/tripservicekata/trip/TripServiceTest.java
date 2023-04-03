5e07d4da84d5138be9340185cfdd04e02481a485
hbentalha <adm-azr-hicham.bentalha@chanel.com>
2023-04-03 12:06:26 +0000

    add test for empty list

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -7,6 +7,8 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class TripServiceTest {
 
   @Test
@@ -19,6 +21,19 @@ public class TripServiceTest {
     };
     assertThatThrownBy(() -> tripService.getTripsByUser(user)).isInstanceOf(
         UserNotLoggedInException.class);
+  }
+
+  @Test
+  void should_return_empty_list_when_loggedUser_has_no_friend() {
+    User user = new User();
+    TripService tripService = new TripService() {
+      User getLoggedUser() {
+        return new User();
+      }
+    };
+    List<Trip> listTrip = tripService.getTripsByUser(user);
+
+    assertThat(listTrip).isEmpty();
   }
 
 }
