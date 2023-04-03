3c57e89d0eb1c42cca325bd79c13e5baa015d503
yaghraba <aghraba.y@gmail.com>
2023-04-03 11:54:48 +0000

    implemented first test

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+  @Test
+  void should_throw_when_no_user_is_logged_in() {
+    User user = new User();
+    TripService tripService = new TripService() {
+      User getLoggedUser() {
+        return null;
+      }
+    };
+    assertThatThrownBy(() -> tripService.getTripsByUser(user)).isInstanceOf(
+        UserNotLoggedInException.class);
+  }
+
 }
